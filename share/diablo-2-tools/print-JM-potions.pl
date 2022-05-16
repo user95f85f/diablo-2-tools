adb13f287b3debb497afd63f5f1e4083483e5f0c
user95f85f <48297795+user95f85f@users.noreply.github.com>
2022-05-16 19:06:40 +0000

    Add files via upload

---
+++ share/diablo-2-tools/print-JM-potions.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature 'say';
+
+
+open my $D2S_LOCATION_CONFIG, '</etc/diablo-2-tools/char-selected.conf' or die $!;
+my $d2s_to_open = <$D2S_LOCATION_CONFIG>;
+close $D2S_LOCATION_CONFIG;
+print("$d2s_to_open:\n");
+open(my ${myd2s}, '<', "$d2s_to_open") or die $!;
+my $d2s_contents = '';
+{
+  local $/;
+  $d2s_contents = <$myd2s>;
+}
+close(${myd2s});
+
+my @JM_locations = ();
+my $pos=0x330;
+while( ($pos = index($d2s_contents, 'JM', $pos)) > -1){
+  push(@JM_locations, $pos+2);
+  $pos++;
+}
+
+for my $i(0 .. $#JM_locations){
+  my $sugar_slice = '';
+  if($i == $#JM_locations){
+    $sugar_slice = substr($d2s_contents, $JM_locations[$i]);
+  }
+  else{
+    $sugar_slice = substr($d2s_contents, $JM_locations[$i], $JM_locations[$i+1] - $JM_locations[$i]);
+  }
+  my $sugar_slice_hex = join('', unpack('H*', $sugar_slice));
+  #XXX potion.....Could be a stamina potion :D
+  if(length($sugar_slice_hex) == 28){
+    # XXX say('Potion ID', sprintf('%03d', $i+1), ' @ addr ', $JM_locations[$i], ': ', $sugar_slice_hex);
+    system('perl', '/usr/share/diablo-2-tools/stam2rejuv.pl', '0x' . $sugar_slice_hex, $JM_locations[$i]);
+    #exit(0);
+  }
+}
+print "\n";
+
